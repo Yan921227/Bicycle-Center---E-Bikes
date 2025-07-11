ed754b239babf04f2db4a6255f5d5571279b998e
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2025-07-11 15:39:03 +0800

    fft_加速_特徵

--- FT_Feature.py
+++ FT_Feature.py
@@ -4,14 +4,14 @@ from scipy.stats import entropy
 import os
 
 # === 步驟 1：讀取傅立葉轉換後的 CSV（輸入）===
-input_path = "output_fft_segments\\下坡\\fft_下坡.csv"  # ← 輸入檔案名稱
+input_path = "output_fft_segments\\加速\\fft_加速.csv"  # ← 輸入檔案名稱
 
 # 使用 os.getcwd() + 多層資料夾結構
 output_folder = os.path.join(os.getcwd(), "output_features", "fft")  # ⬅️ 可以自訂資料夾結構
 os.makedirs(output_folder, exist_ok=True)  # 自動建立資料夾（如果不存在）
 
 # 最終輸出檔案位置
-output_path = os.path.join(output_folder, "fft_下坡_特徵.csv")
+output_path = os.path.join(output_folder, "fft_加速_特徵.csv")
 
 # 驗證輸入檔案是否存在
 if not os.path.exists(input_path):
@@ -29,7 +29,7 @@ print("📂 輸入資料已讀取，共有筆數：", len(fft_df))
 
 
 # === 步驟 2：智慧特徵擷取器定義 ===
-def smart_fft_feature_selector(df, low_freq_max=0.50, mid_freq_max=78.00, high_freq_min=155.50, top_n=3):
+def smart_fft_feature_selector(df, low_freq_max=4.00, mid_freq_max=61.50, high_freq_min=119.00, top_n=3):
     features = []
 
     for (label, axis), group in df.groupby(['label', 'axis']):
